5c52416f3436c2ac95cc89ae1adb1b693f67ad6f
Jennifer Law <jenniferlaw@Jennifers-MacBook-Air-2.local>
2021-10-14 15:01:34 -0700

    exercise 1,2,3 complete

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,16 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.where("id = '1'")
+@store2 = Store.where("id = '2'")
+
+@store1.update(1, name: 'Burnaby123')
+
+# one record
+puts "store: #{Store.find_by(id: '2').name}"
+
+# array return by where method
+@store1.each do |store| 
+puts store.name 
+end
